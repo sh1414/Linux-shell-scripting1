076841a811bd5d19c933434e13ab53ae308584df
sh1414 <49478603+sh1414@users.noreply.github.com>
2026-01-02 18:25:32 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -19,7 +19,6 @@ class Handler(BaseHTTPRequestHandler):
             </body>
         </html>
         """.encode())
-
 server = HTTPServer(("0.0.0.0", 8080), Handler)
 print("Server running on port 8080...")
 server.serve_forever()
